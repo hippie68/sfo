2923d123e9c73e877fd1d2c117439e2538f717b9
hippie68 <65259318+hippie68@users.noreply.github.com>
2021-07-02 22:21:25 +0200

    Implemented write access and options

--- sfo.c
+++ sfo.c
@@ -1,21 +1,41 @@
-/* Prints SFO information of either a PS4 PKG or param.sfo file.
+/* Prints and modifies SFO information of either a PS4 PKG or param.sfo file.
  * Made with info from https://www.psdevwiki.com/ps4/Param.sfo.
  * Get updates and Windows binaries at https://github.com/hippie68/sfo. */
 
+#include <ctype.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+// Global variables
+FILE *file;
+long int pkg_offset = 0;
+
+// Default options
+int option_decimal = 0;
+int option_debug = 0;
+int option_search = 0;
+int option_verbose = 0;
+int option_write = 0;
+
+// Shows usage and help
 void show_help(char *program_name) {
   printf(
-  "Usage: %s file [search]\n"
-  "Prints SFO information of either a PS4 PKG or param.sfo file.\n"
-  "Providing a search string will output the value of that specific key only.\n",
+  "Usage: %s [-dDhvw] file [search] [replace]\n\n"
+  "  Prints SFO information of either a PS4 PKG or param.sfo file.\n"
+  "  The search string is case-insensitive.\n"
+  "  To search and replace, write access must be explicitly enabled (option -w).\n"
+  "  Doing so without specifying [replace] will write an empty string\n"
+  "  or the integer value 0.\n\n"
+  "  Options:\n"
+  "    -d  Display integer values as decimal numerals\n"
+  "    -D  Print debug information\n"
+  "    -h  Display this help info\n"
+  "    -v  Increase verbosity\n"
+  "    -w  Enable write access\n\n",
   program_name);
 }
-
-FILE *file;
-long long int pkg_offset = 0;
 
 // Replacement function for byteswap.h's bswap_16
 uint16_t bswap_16(uint16_t val) {
@@ -28,8 +48,16 @@ uint32_t bswap_32(uint32_t val) {
   return (val << 16) | (val >> 16);
 }
 
+// Converts a string to lowercase
+void toupper_string(char *string) {
+  while (*string) {
+    string[0] = toupper(string[0]);
+    string++;
+  }
+}
+
 // Returns "length" bytes, starting at specified offset
-int getbytes(long long int offset, int length) {
+int getbytes(long int offset, int length) {
   int bytes;
   fseek(file, pkg_offset + offset, SEEK_SET);
   fread(&bytes, length, 1, file);
@@ -47,49 +75,103 @@ int get_pkg_offset() {
     uint32_t size;
     uint64_t padding;
   } pkg_table_entry;
-  int i;
-  int pkg_file_count = bswap_32(getbytes(0x00C, 4));
   int pkg_table_offset = bswap_32(getbytes(0x018, 4));
-
+  int pkg_file_count = bswap_32(getbytes(0x00C, 4));
   fseek(file, pkg_table_offset, SEEK_SET);
-  for (i = 0; i < pkg_file_count; i++) {
-    fseek(file, 32, SEEK_CUR);
+  for (int i = 0; i < pkg_file_count; i++) {
     fread(&pkg_table_entry, sizeof(struct pkg_table_entry), 1, file);
     if (bswap_32(pkg_table_entry.id) == 4096 ) { // param.sfo ID
       return bswap_32(pkg_table_entry.offset);
     }
   }
-  printf("Could not find param.sfo inside PKG file.");
+  printf("ERROR: Could not find param.sfo inside PKG file.\n");
   exit(1);
 }
 
 int main(int argc, char *argv[])
 {
-  char *filename;
-  if (argc > 1) {
-    filename = argv[1];
-  } else {
-    show_help(argv[0]);
-    return 1;
+  // Parse command line options
+  char *filename, *search, *replace;
+  {
+    char *arg[3] = {"", "", ""};
+    if (argc > 1) {
+      int j = 0;
+      for (int i = 1; i < argc; i++) {
+        if (argv[i][0] == '-') {
+          for (int o = 1; argv[i][o] != '\0'; o++) {
+            switch (argv[i][o]) {
+              case 'd':
+                option_decimal = 1;
+                break;
+              case 'D':
+                option_debug = 1;
+                break;
+              case 'h':
+                show_help(argv[0]);
+                break;
+              case 'v':
+                option_verbose = 1;
+                break;
+              case 'w':
+                option_write = 1;
+                break;
+              default:
+                printf("ERROR: Unknown option: %c\n", argv[i][o]);
+            }
+          }
+          argv[i][0] = 0;
+        } else {
+          arg[j++] = argv[i];
+        }
+      }
+    } else {
+      show_help(argv[0]);
+      return 1;
+    }
+    filename = arg[0];
+    if (strlen(arg[1]) > 0) {
+      search = arg[1];
+      toupper_string(search);
+      option_search = 1;
+    }
+    replace = arg[2];
+
+    // DEBUG: Print all command line arguments and options
+    if (option_debug == 1) {
+      for (int i = 0; i < sizeof(arg) / sizeof(arg[0]); i++) {
+        printf("DEBUG: arg[%d]: %s\n", i, arg[i]);
+      }
+      printf("DEBUG: search: %s\n", search);
+      printf("DEBUG: replace: %s\n", replace);
+      printf("DEBUG: option_decimal: %d\n", option_decimal);
+      printf("DEBUG: option_debug: %d\n", option_debug);
+      printf("DEBUG: Option search: %d\n", option_search);
+      printf("DEBUG: option_verbose: %d\n", option_verbose);
+      printf("DEBUG: Option write: %d\n", option_write);
+    }
   }
 
   // Open binary file
-  file = fopen(filename, "rb");
+  if (option_write == 1) {
+    file = fopen(filename, "r+b"); // Read/write
+  } else {
+    file = fopen(filename, "rb"); // Read only
+  }
   if (file == NULL) {
-    fprintf(stderr, "ERROR: Could not open file \"%s\".", filename);
+    fprintf(stderr, "ERROR: Could not open file \"%s\".\n", filename);
     return 1;
   }
 
   // Set PKG offset if file is a PKG
-  int magic = bswap_32(getbytes(0, 4));
-  if (magic == 2135117396) { // PKG file
+  int magic = getbytes(0, 4);
+  if (magic == 1414415231) { // PKG file
     pkg_offset = get_pkg_offset();
-    magic = bswap_32(getbytes(0, 4));
+    magic = getbytes(0, 4);
   }
 
   // Exit if param.sfo not found
-  if (magic != 5264198) {
-    fprintf(stderr, "ERROR: param.sfo magic not found.\n");
+  if (magic != 1179865088) { // Param.sfo file
+    fprintf(stderr, "ERROR: Param.sfo magic byte not found.\n");
     return 1;
   }
 
@@ -104,7 +186,17 @@ int main(int argc, char *argv[])
   fseek(file, pkg_offset, SEEK_SET);
   fread(&sfo_header, sizeof(struct sfo_header), 1, file);
 
-  // Loop through the index table
+  // Print header info if verbose
+  if (option_verbose == 1) {
+    char *version = malloc(9);
+    sprintf(version, "%X", sfo_header.version);
+    printf("Param.sfo version: %c.%c%c\nNumber of parameters: %d\n",
+      version[0], version[1], version[2], sfo_header.indextable_entries);
+  }
+
+  // Define the index table
+  const int indextable_offset = 0x14;
+  const int indextable_entry_size = 0x10;
   struct indextable_entry {
     uint16_t keytable_offset;
     uint16_t param_fmt; // Type of data
@@ -112,38 +204,112 @@ int main(int argc, char *argv[])
     uint32_t parameter_max_length;
     uint32_t datatable_offset;
   } indextable_entry[sfo_header.indextable_entries];
-  int i, integer = 0;
-  for (i = 0; i < sfo_header.indextable_entries; i++) {
-    fseek(file, pkg_offset + 0x14 + i * 0x10, SEEK_SET);
+
+  // Loop starts here
+  int integer = 0;
+  for (int i = 0; i < sfo_header.indextable_entries; i++) {
+    fseek(file, pkg_offset + indextable_offset + i * indextable_entry_size, SEEK_SET);
     fread(&indextable_entry[i], sizeof(struct indextable_entry), 1, file);
 
-    // Get key name
+    // Get current parameter's key name
     char *key;
     key = malloc(100);
     fseek(file, pkg_offset + sfo_header.keytable_offset + indextable_entry[i].keytable_offset, SEEK_SET);
     fread(key, 100, 1, file);
 
-    // Get data
-    fseek(file, pkg_offset + sfo_header.datatable_offset + indextable_entry[i].datatable_offset, SEEK_SET);
-    switch (indextable_entry[i].param_fmt) {
-      case 516: // UTF-8 string
-        ; // Empty statement required by the C standard
-        char *string;
-        string = malloc(indextable_entry[i].parameter_length);
-        fread(string, (indextable_entry[i].parameter_length), 1, file);
-        printf("%s=%s\n", key, string);
-        free(string);
-        break;
-      case 1028: // Integer
-        fread(&integer, 4, 1, file);
-        printf("%s=0x%08x\n", key, integer);
-        break;
-      default:
-        printf("[UNKNOWN DATA TYPE: %04X]\n", bswap_16(indextable_entry[i].param_fmt));
+    // Continue loop if key does not match the (optional) search term
+    if (option_search == 0) {
+      // Print current parameter's index
+      if (option_verbose == 1) {
+        printf("[%02d] ", i);
+      }
+      // Print current parameter's key
+      printf("%s=", key);
+    } else if (strcmp(search, key) != 0) {
+      continue;
+    }
+
+    // Write
+    if (option_write == 1) {
+      if (strcmp(search, key) == 0) {
+        switch (indextable_entry[i].param_fmt) {
+          case 516: // UTF-8 string
+            // Check new string for valid length
+            if (strlen(replace) > indextable_entry[i].parameter_max_length - 1) {
+              fprintf(stderr, "ERROR: Replacement string \"%s\" too large (%d characters allowed).\n", replace, indextable_entry[i].parameter_max_length - 1);
+              return 0;
+            }
+            // Fill space with zeros
+            char *string;
+            string = malloc(indextable_entry[i].parameter_max_length);
+            memset(string, '\0', indextable_entry[i].parameter_max_length);
+            fseek(file, pkg_offset + sfo_header.datatable_offset + indextable_entry[i].datatable_offset, SEEK_SET);
+            fwrite(string, indextable_entry[i].parameter_max_length, 1, file);
+            free(string);
+            // Write new string to file
+            fseek(file, pkg_offset + sfo_header.datatable_offset + indextable_entry[i].datatable_offset, SEEK_SET);
+            fwrite(replace, strlen(replace), 1, file);
+            // Write new parameter length to file
+            indextable_entry[i].parameter_length = strlen(replace) + 1;
+            fseek(file, pkg_offset + indextable_offset + i * indextable_entry_size, SEEK_SET);
+            fwrite(&indextable_entry[i], indextable_entry_size, 1, file);
+            break;
+          case 1028: // Integer
+            fseek(file, pkg_offset + sfo_header.datatable_offset + indextable_entry[i].datatable_offset, SEEK_SET);
+            uint32_t temp;
+            if (strlen(replace) > 1 && (replace[1] == 'x' || replace[1] == 'X')) {
+              sscanf(replace, "%x", &temp);
+            } else {
+              temp = atoi(replace);
+            }
+            fwrite(&temp, 4, 1, file);
+            break;
+          default:
+            fprintf(stderr, "ERROR: Unknown data type: %04X\n", bswap_16(indextable_entry[i].param_fmt));
+            return 1;
+        }
+        fclose(file); // Writes the buffer
+        return 0;
+      }
+    // Read
+    } else {
+      switch (indextable_entry[i].param_fmt) {
+        case 516: // UTF-8 string
+          fseek(file, pkg_offset + sfo_header.datatable_offset + indextable_entry[i].datatable_offset, SEEK_SET);
+          char *string;
+          string = malloc(indextable_entry[i].parameter_length);
+          fread(string, indextable_entry[i].parameter_length, 1, file);
+          if (option_verbose == 1) {
+            printf("\"%s\" (%d/%d bytes)\n", string, indextable_entry[i].parameter_length, indextable_entry[i].parameter_max_length);
+          } else {
+            printf("%s\n", string);
+          }
+          free(string);
+          break;
+        case 1028: // Integer
+          fseek(file, pkg_offset + sfo_header.datatable_offset + indextable_entry[i].datatable_offset, SEEK_SET);
+          fread(&integer, 4, 1, file);
+          if (option_decimal == 1) {
+            if (option_verbose == 1) {
+              printf("%d (%d bytes)\n", integer, indextable_entry[i].parameter_length);
+            } else {
+              printf("%d\n", integer);
+            }
+          } else {
+            if (option_verbose == 1) {
+              printf("0x%08X (%d bytes)\n", integer, indextable_entry[i].parameter_length);
+            } else {
+              printf("0x%08x\n", integer);
+            }
+          }
+          break;
+        default:
+          fprintf(stderr, "ERROR: Unknown data type: %04X\n", bswap_16(indextable_entry[i].param_fmt));
+      }
     }
     free(key);
   }
 
-  // Close the file
   fclose(file);
+  return 0;
 }
