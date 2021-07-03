7d57085595540c020991e5f7dfec5609281ee5b5
hippie68 <65259318+hippie68@users.noreply.github.com>
2021-07-03 21:55:52 +0200

    Bug fixes

--- sfo.c
+++ sfo.c
@@ -1,4 +1,4 @@
-/* Prints and modifies SFO information of either a PS4 PKG or param.sfo file.
+/* Prints or modifies SFO parameters of a PS4 PKG or a param.sfo file.
  * Made with info from https://www.psdevwiki.com/ps4/Param.sfo.
  * Get updates and Windows binaries at https://github.com/hippie68/sfo. */
 
@@ -14,27 +14,40 @@ long int pkg_offset = 0;
 
 // Default options
 int option_decimal = 0;
-int option_debug = 0;
 int option_search = 0;
 int option_verbose = 0;
 int option_write = 0;
 
+// Returns a filename without its path
+char *basename(char *filename) {
+  #if defined(_WIN32) || defined(_WIN64)
+  char *base = strrchr(filename, '\\');
+  #else
+  char *base = strrchr(filename, '/');
+  #endif
+  if (base != NULL && strlen(base) > 1) {
+    base++;
+    return base;
+  } else {
+    return filename;
+  }
+}
+
 // Shows usage and help
 void show_help(char *program_name) {
   printf(
-  "Usage: %s [-dDhvw] file [search] [replace]\n\n"
-  "  Prints SFO information of either a PS4 PKG or param.sfo file.\n"
-  "  The search string is case-insensitive.\n"
-  "  To search and replace, write access must be explicitly enabled (option -w).\n"
-  "  Doing so without specifying [replace] will write an empty string\n"
-  "  or the integer value 0.\n\n"
+  "Usage: %s [-dhvw] file [search] [replace]\n\n"
+  "  Prints or modifies SFO parameters of a PS4 PKG or a param.sfo file.\n"
+  "  The [search] string is case-insensitive.\n"
+  "  To modify parameters, write access must be explicitly enabled (option -w).\n"
+  "  Enabling write access without specifying a [replace] string will write\n"
+  "  an empty string or the integer value \"0\".\n\n"
   "  Options:\n"
   "    -d  Display integer values as decimal numerals\n"
-  "    -D  Print debug information\n"
   "    -h  Display this help info\n"
   "    -v  Increase verbosity\n"
-  "    -w  Enable write access\n\n",
-  program_name);
+  "    -w  Enable write access\n",
+  basename(program_name));
 }
 
 // Replacement function for byteswap.h's bswap_16
@@ -48,7 +61,7 @@ uint32_t bswap_32(uint32_t val) {
   return (val << 16) | (val >> 16);
 }
 
-// Converts a string to lowercase
+// Converts a string to uppercase
 void toupper_string(char *string) {
   while (*string) {
     string[0] = toupper(string[0]);
@@ -64,8 +77,10 @@ int getbytes(long int offset, int length) {
   return bytes;
 }
 
-// Finds the param.sfo's offset inside a PKG file
-int get_pkg_offset() {
+// Finds the param.sfo's offset inside a PS4 PKG file
+int get_ps4_pkg_offset() {
+  const int pkg_table_offset = bswap_32(getbytes(0x018, 4));
+  const int pkg_file_count = bswap_32(getbytes(0x00C, 4));
   struct pkg_table_entry {
     uint32_t id;
     uint32_t filename_offset;
@@ -74,40 +89,37 @@ int get_pkg_offset() {
     uint32_t offset;
     uint32_t size;
     uint64_t padding;
-  } pkg_table_entry;
-  int pkg_table_offset = bswap_32(getbytes(0x018, 4));
-  int pkg_file_count = bswap_32(getbytes(0x00C, 4));
+  } pkg_table_entry[pkg_file_count];
   fseek(file, pkg_table_offset, SEEK_SET);
+  fread(pkg_table_entry, sizeof(struct pkg_table_entry) * pkg_file_count, 1, file);
   for (int i = 0; i < pkg_file_count; i++) {
-    fread(&pkg_table_entry, sizeof(struct pkg_table_entry), 1, file);
-    if (bswap_32(pkg_table_entry.id) == 4096 ) { // param.sfo ID
-      return bswap_32(pkg_table_entry.offset);
+    if (pkg_table_entry[i].id == 1048576) { // param.sfo ID
+      return bswap_32(pkg_table_entry[i].offset);
     }
   }
-  printf("ERROR: Could not find param.sfo inside PKG file.\n");
+  fprintf(stderr, "ERROR: Could not find a param.sfo file inside the PS4 PKG.\n");
+  fclose(file);
   exit(1);
 }
 
 int main(int argc, char *argv[])
 {
-  // Parse command line options
   char *filename, *search, *replace;
+
+  // Parse command line arguments
   {
-    char *arg[3] = {"", "", ""};
+    char *arg[3] = {"", "", ""}; // Non-option arguments
     if (argc > 1) {
-      int j = 0;
-      for (int i = 1; i < argc; i++) {
+      for (int i = 1, j = 0; i < argc; i++) {
         if (argv[i][0] == '-') {
           for (int o = 1; argv[i][o] != '\0'; o++) {
             switch (argv[i][o]) {
               case 'd':
                 option_decimal = 1;
                 break;
-              case 'D':
-                option_debug = 1;
-                break;
               case 'h':
                 show_help(argv[0]);
+                return 0;
                 break;
               case 'v':
                 option_verbose = 1;
@@ -116,11 +128,12 @@ int main(int argc, char *argv[])
                 option_write = 1;
                 break;
               default:
-                printf("ERROR: Unknown option: %c\n", argv[i][o]);
+                fprintf(stderr, "ERROR: Unknown option: %c\n", argv[i][o]);
+                return 1;
             }
           }
-          argv[i][0] = 0;
         } else {
+          // Track non-option arguments
           arg[j++] = argv[i];
         }
       }
@@ -128,26 +141,26 @@ int main(int argc, char *argv[])
       show_help(argv[0]);
       return 1;
     }
+
+    // Assign non-option arguments
     filename = arg[0];
-    if (strlen(arg[1]) > 0) {
-      search = arg[1];
+    search = arg[1];
+    if (strlen(search) > 0) {
       toupper_string(search);
       option_search = 1;
     }
     replace = arg[2];
 
-    // DEBUG: Print all command line arguments and options
-    if (option_debug == 1) {
-      for (int i = 0; i < sizeof(arg) / sizeof(arg[0]); i++) {
-        printf("DEBUG: arg[%d]: %s\n", i, arg[i]);
-      }
-      printf("DEBUG: search: %s\n", search);
-      printf("DEBUG: replace: %s\n", replace);
-      printf("DEBUG: option_decimal: %d\n", option_decimal);
-      printf("DEBUG: option_debug: %d\n", option_debug);
-      printf("DEBUG: Option search: %d\n", option_search);
-      printf("DEBUG: option_verbose: %d\n", option_verbose);
-      printf("DEBUG: Option write: %d\n", option_write);
+    // Sanity checks for enabled write access
+    if (option_write == 0 && strlen(replace) > 0) {
+      fprintf(stderr, "ERROR: You specified [replace] without enabling write access (option -w).\n\n");
+      show_help(argv[0]);
+      return 1;
+    }
+    if (option_write == 1 && strlen(search) == 0) {
+      fprintf(stderr, "ERROR: You enabled write access (option -w) without specifying a search string.\n\n");
+      show_help(argv[0]);
+      return 1;
     }
   }
 
@@ -164,14 +177,14 @@ int main(int argc, char *argv[])
 
   // Set PKG offset if file is a PKG
   int magic = getbytes(0, 4);
-  if (magic == 1414415231) { // PKG file
-    pkg_offset = get_pkg_offset();
+  if (magic == 1414415231) { // PS4 PKG file
+    pkg_offset = get_ps4_pkg_offset();
     magic = getbytes(0, 4);
   }
 
   // Exit if param.sfo not found
   if (magic != 1179865088) { // Param.sfo file
-    fprintf(stderr, "ERROR: Param.sfo magic byte not found.\n");
+    fprintf(stderr, "ERROR: Param.sfo magic number not found.\n");
     return 1;
   }
 
@@ -213,9 +226,9 @@ int main(int argc, char *argv[])
 
     // Get current parameter's key name
     char *key;
-    key = malloc(100);
+    key = malloc(50);
     fseek(file, pkg_offset + sfo_header.keytable_offset + indextable_entry[i].keytable_offset, SEEK_SET);
-    fread(key, 100, 1, file);
+    fread(key, 50, 1, file);
 
     // Continue loop if key does not match the (optional) search term
     if (option_search == 0) {
@@ -226,6 +239,7 @@ int main(int argc, char *argv[])
       // Print current parameter's key
       printf("%s=", key);
     } else if (strcmp(search, key) != 0) {
+      free(key);
       continue;
     }
 
@@ -234,9 +248,11 @@ int main(int argc, char *argv[])
       if (strcmp(search, key) == 0) {
         switch (indextable_entry[i].param_fmt) {
           case 516: // UTF-8 string
+            ;
             // Check new string for valid length
-            if (strlen(replace) > indextable_entry[i].parameter_max_length - 1) {
-              fprintf(stderr, "ERROR: Replacement string \"%s\" too large (%d characters allowed).\n", replace, indextable_entry[i].parameter_max_length - 1);
+            int string_length = strlen(replace);
+            if (string_length > indextable_entry[i].parameter_max_length - 1) {
+              fprintf(stderr, "ERROR: Replacement string \"%s\" too large (%d/%d characters).\n", replace, string_length, indextable_entry[i].parameter_max_length - 1);
               return 0;
             }
             // Fill space with zeros
@@ -280,7 +296,7 @@ int main(int argc, char *argv[])
           string = malloc(indextable_entry[i].parameter_length);
           fread(string, indextable_entry[i].parameter_length, 1, file);
           if (option_verbose == 1) {
-            printf("\"%s\" (%d/%d bytes)\n", string, indextable_entry[i].parameter_length, indextable_entry[i].parameter_max_length);
+            printf("\"%s\" (%d/%d bytes UTF-8 string)\n", string, indextable_entry[i].parameter_length, indextable_entry[i].parameter_max_length);
           } else {
             printf("%s\n", string);
           }
@@ -291,15 +307,15 @@ int main(int argc, char *argv[])
           fread(&integer, 4, 1, file);
           if (option_decimal == 1) {
             if (option_verbose == 1) {
-              printf("%d (%d bytes)\n", integer, indextable_entry[i].parameter_length);
+              printf("%d (%d bytes unsigned integer)\n", integer, indextable_entry[i].parameter_length);
             } else {
               printf("%d\n", integer);
             }
           } else {
             if (option_verbose == 1) {
-              printf("0x%08X (%d bytes)\n", integer, indextable_entry[i].parameter_length);
+              printf("0x%08X (%d bytes unsigned integer)\n", integer, indextable_entry[i].parameter_length);
             } else {
-              printf("0x%08x\n", integer);
+              printf("0x%08X\n", integer);
             }
           }
           break;
